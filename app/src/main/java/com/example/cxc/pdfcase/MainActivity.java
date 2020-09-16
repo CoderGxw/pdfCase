f7ac401212575f954a59e522cbc03e4fe08c894c
guoxw <xuewei_g@163.com>
2020-09-16 15:38:10 +0800

    修改配置文件和模板

--- app/src/main/java/com/example/cxc/pdfcase/MainActivity.java
+++ app/src/main/java/com/example/cxc/pdfcase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cxc.pdfcase;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
@@ -8,6 +9,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -29,11 +31,13 @@ import com.itextpdf.text.pdf.PdfGState;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -66,7 +70,12 @@ public class MainActivity extends AppCompatActivity {
                 if (!new File(path, SrcFileName).exists()) {
                     Toast.makeText(mContext, "文件不存在", Toast.LENGTH_SHORT).show();
                 }
-                mPdfView.fromFile(new File(path,SrcFileName))
+                File dir = getExternalFilesDir("text");
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                File file = new File(dir, SrcFileName);
+                mPdfView.fromFile(file)
                         .enableSwipe(true)
                         .enableAnnotationRendering(true)
                         .spacing(10) // in dp
@@ -99,22 +108,68 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-    public static final String SrcFileName = "pdfmodel.pdf";
+    public static final String path = Environment.getExternalStorageDirectory().getAbsolutePath()+ File.separator+"imageCompressTemp/";
+    public static final String SrcFileName = "7model.pdf";
 
     private void convertPDFInfo( String timeWater, BaseFont bfChinese) throws IOException, DocumentException {
-        InputStream open = mContext.getClass().getClassLoader().getResourceAsStream("assets/" +SrcFileName );
+        File dir = getExternalFilesDir("text");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+
+        //获取assets资源管理器
+        AssetManager assetManager = this.getAssets();
+        InputStream open = assetManager.open(SrcFileName);
         PdfReader reader = new PdfReader(open);
-        Log.d("tag",path);
-        PdfStamper pdfStamper = new PdfStamper(reader, new FileOutputStream(new File(path, SrcFileName)));
+        File file = new File(dir, SrcFileName);
+        Log.d("TAG", "convertPDFInfo: ");
+        PdfStamper pdfStamper = new PdfStamper(reader, new FileOutputStream(file));
         AcroFields acroFields = pdfStamper.getAcroFields();
 
 
 
         Map<String, String> formData = new HashMap<>();
-        formData.put("text1", "我是填充内容");
-
-
+        formData.put("name", "周青");
+        formData.put("cym", "张三");
+        formData.put("sfz", "110244199203065665");
+        formData.put("xb", "女");
+        formData.put("mz", "汉");
+        formData.put("csrq", "1995年02月");
+        formData.put("whcd", "高中");
+        formData.put("jkzk", "良好");
+        formData.put("zzmm", "群众");
+        formData.put("hyzk", "已婚");
+        formData.put("jzd", "北京市海淀区北四环中路211号");
+        formData.put("hjd", "北京市海淀区北四环中路211号华北所科技大厦");
+        formData.put("zxd", "北京市海淀区学院路司法所");
+        formData.put("xgzdw", "中国电子科技集团公司第十五研究所");
+        formData.put("dwlxdh", "010-89021552");
+        formData.put("grlxdh", "13904446658");
+        formData.put("zm", "贪污罪");
+        formData.put("xz", "我也不知道填什么");
+        formData.put("ypxq", "一年零四个月缓期六个月");
+        formData.put("sqjzjdjg", "海淀区人民法院");
+        formData.put("yjycs", "海淀区看守所");
+        formData.put("jzl", "禁止进入北京二环");
+        formData.put("jzl_s_year", "2020");
+        formData.put("jzl_e_year", "2020");
+        formData.put("jzl_s_month", "01");
+        formData.put("jzl_e_month", "12");
+        formData.put("jzl_s_day", "01");
+        formData.put("jzl_e_day", "01");
+        formData.put("jzlb", "暂监外病检");
+        formData.put("jzqx", "一年零六个月");
+        formData.put("jz_s_year", "2020");
+        formData.put("jz_e_year", "2020");
+        formData.put("jz_s_month", "01");
+        formData.put("jz_e_month", "12");
+        formData.put("jz_s_day", "01");
+        formData.put("jz_e_day", "01");
+        formData.put("zyfzss", "2018年至案发前，被告人周青在无烟草专卖零售许可证的情况下，在本市海淀区天秀路99号-2层50号零售商铺内出售烟草制品。2020年1月10日，公安机关从上述地点起获待销售烟草共计39种，其中雪茄烟共计36种535支。被告人周青于当日被公安机关抓获归案，后如实供述了上述犯罪事实。");
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        String img = BitmapToStrByBase64(bitmap);
+        formData.put("faceImg", img);
         for (Iterator it = formData.keySet().iterator(); it.hasNext(); ) {
             String key = (String) it.next();
             String value = (String) formData.get(key);
@@ -153,7 +208,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    public String BitmapToStrByBase64(Bitmap bit){
+        ByteArrayOutputStream bos=new ByteArrayOutputStream();
+        bit.compress(Bitmap.CompressFormat.JPEG, 100, bos);//参数100表示不压缩
+        byte[] bytes=bos.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
     /**
      * 加水印
      *
